b5fd190504779682c12a6d1cf003f42a3d349bbf
Virginia Gichira <gichiravirginia@gmail.com>
2024-03-30 14:56:43 +0300

    add

---
+++ index.js
@@ -0,0 +1,17 @@
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+const div = divide(2000, 100);
+
+console.log(divide);
+
+//Arrow function that returns the square of the number
+const square = (x) => x ** 2;
+
+console.log(square(2));
+
+//Arrow funtion add that adds together two paramenters
+
+const add = (a, b) => a + b;
+console.log(add(3, 4));
